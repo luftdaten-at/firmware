c2ab75165aa319b70646850cc3a0756dadab92f1
Nik Sauer <nik.sauer@luftdaten.at>
2024-09-24 08:32:42 +0200

    add-set-longitude-latitude-hight

--- firmware/models/air_station.py
+++ firmware/models/air_station.py
@@ -11,6 +11,9 @@ class AirStation(LdProductModel):
         self.wifi = wifi
         self.new_ssid = None
         self.new_password = None
+        self.longitude = None
+        self.latitude = None
+        self.hight = None
         
     def receive_command(self, command):
         if len(command) == 0:
@@ -19,16 +22,24 @@ class AirStation(LdProductModel):
         cmd, *data = command
         if cmd == BleCommands.SET_WIFI_SSID:
             self.new_ssid = bytearray(data).decode('utf-8')
-            print(self.new_ssid)
 
         if cmd == BleCommands.SET_WIFI_PASSWORD:
             self.new_password = bytearray(data).decode('utf-8')
-            print(self.new_password)
+
+        if cmd == BleCommands.SET_LONGITUDE:
+            self.longitude = bytearray(data).decode('utf-8')
+
+        if cmd == BleCommands.SET_LATITUDE:
+            self.latitude = bytearray(data).decode('utf-8')
+        
+        if cmd == BleCommands.SET_HIGHT:
+            self.hight = bytearray(data).decode('utf-8')
 
         if self.new_ssid and self.new_password:
             self.wifi.connect(self.new_ssid, self.new_password)
             self.new_ssid = None
             self.new_password = None
+        
 
     def receive_button_press(self):
         self.ble_on = not self.ble_on
@@ -40,8 +51,34 @@ class AirStation(LdProductModel):
         self.status_led.show()
     
     def tick(self):
-        pass
-        # We measure in fixed intervals, do this here
-        
+        if not any(self.longitude, self.latitude, self.hight):
+            print('DATA CANNOT BE TRANSMITTED')
+            print('Not all configurations have been made')
+            return 
+        '''
+        [
+        [
+            {
+                "time": "2024-04-29T08:25:20.766Z",
+                "device": "string",
+                "location": {
+                    "lat": 0,
+                    "lon": 0,
+                    "hight": 0
+            },
+            {
+                "sen1" : {"dim": "val", "dim2": "val2"},
+                "sen2" : {"dim": "val", "dim2": "val2"}
+            }
+        ]
+        ] 
+        '''
+        for sensor in self.sensors:
+            try:
+                sensor.read()
+            except:
+                print(f"Error reading sensor {sensor.model_id}, using previous values")
+            print(sensor.current_values)
+
     def connection_update(self, connected):
         pass
