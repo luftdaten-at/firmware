93626c9bd76317295300d814f5292534b3a98e4c
Nik Sauer <nik.sauer@luftdaten.at>
2024-11-06 12:51:54 +0100

    removed data suffix from api url, save logs in json_queue

--- firmware/logger.py
+++ firmware/logger.py
@@ -32,8 +32,16 @@ class SimpleLogger:
             }
             storage.remount('/', False)
             with open('log.txt', 'a') as f:
-                print(json.dumps(log_entry), file=f)
+                print(log_message, file=f)
             storage.remount('/', True)
+
+            self.save(log_entry)
+
+    def save(self, data):
+        storage.remount('/', False)
+        with open('json_queue/tmp_log.txt', 'a') as f:
+            print(json.dumps(data), file=f)
+        storage.remount('/', True)
 
     def debug(self, *args):
         self.log(self.format_message(*args), 'DEBUG')
