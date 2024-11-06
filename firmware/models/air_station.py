35367b15cdf8ac302a5d126e48205a991d72fc33
Nik Sauer <nik.sauer@luftdaten.at>
2024-11-06 13:10:53 +0100

    send status to api

--- firmware/models/air_station.py
+++ firmware/models/air_station.py
@@ -240,7 +240,7 @@ class AirStation(LdProductModel):
                     data = self.get_info()
                     data["status_list"] = status_list
 
-                    response = WifiUtil.send_json_to_api(data)
+                    response = WifiUtil.send_json_to_api(data, router='status')
                     logger.debug(f'{file_path=}')
                     logger.debug(f'API Response: {response.status_code}')
                     logger.debug(f'API Response: {response.text}')
