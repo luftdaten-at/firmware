846af2325b8030698438783b5a23ef003dc42ee4
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-07 14:17:25 +0100

    send to additional api urls

--- firmware/wifi_client.py
+++ firmware/wifi_client.py
@@ -88,6 +88,15 @@ class WifiUtil:
             url=f"{Config.runtime_settings['API_URL']}/{router}",
             json=data
         )
+        # send to additional APIs
+        # TODO: Handle response
+        if Config.settings['API_URLS']:
+            for api_url in Config.settings['API_URLS']:
+                WifiUtil.api_session.request(
+                    method='POST',
+                    url=f"{api_url}/{router}",
+                    json=data
+                )
         return response
  
 
