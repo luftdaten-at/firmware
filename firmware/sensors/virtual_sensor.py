ccd8994974641baad18d1e9b109d6095d6b541c1
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-08 12:54:23 +0100

    bug fix

--- firmware/sensors/virtual_sensor.py
+++ firmware/sensors/virtual_sensor.py
@@ -1,4 +1,3 @@
-import statistics
 from sensors.sensor import Sensor
 from enums import SensorModel, Dimension
 
@@ -14,7 +13,7 @@ class VirtualSensor(Sensor):
         self.model_id = SensorModel.VIRTUAL_SENSOR
         self.required_sensor_dict = required_sensor_dict
         self.calculated_dimension_set = calculated_dimension_set        
-        self.current_values: dict[Dimension, float] = None
+        self.current_values: dict[Dimension, float] = {}
 
     def read(self) -> None:
         '''
@@ -24,6 +23,9 @@ class VirtualSensor(Sensor):
         # update all sensors
         for sen in self.required_sensor_dict.values():
             sen.read()
+        
+        def mean(l: list):
+            return sum(l) / len(l)
 
         if Dimension.ADJUSTED_TEMP_CUBE in self.calculated_dimension_set:
             # check if we have all required sensor
@@ -31,9 +33,9 @@ class VirtualSensor(Sensor):
             need = Dimension.get_required_sensors(Dimension.ADJUSTED_TEMP_CUBE)
             if need <= set(self.required_sensor_dict.keys()):
                 # by default return the mean tempreature
-                statistics.mean([ 
+                self.current_values[Dimension.ADJUSTED_TEMP_CUBE] = mean([ 
                     sen.current_values[Dimension.TEMPERATURE]
-                    for model_id, sen in self.required_sensor_dict
+                    for model_id, sen in self.required_sensor_dict.items()
                         if model_id in need
                 ])
             else:
