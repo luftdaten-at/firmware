ec937fc85221e2383de89cb8efe69ff47cb09fea
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-17 13:57:33 +0100

    added sensor class for ltr390

--- firmware/sensors/sensor_ltr390.py
+++ firmware/sensors/sensor_ltr390.py
@@ -8,33 +8,33 @@ class Sht31Sensor(Sensor):
         super().__init__()
         self.model_id = SensorModel.LTR390
         self.measures_values = [
-            Dimension.TEMPERATURE,
-            Dimension.HUMIDITY,
+            Dimension.UVS,
+            Dimension.LIGHT
         ]
         self.current_values = {
-            Dimension.TEMPERATURE: None,
-            Dimension.HUMIDITY: None,
+            Dimension.UVS: None,
+            Dimension.LIGHT: None,
         }
         self.value_quality = {
-            Dimension.TEMPERATURE: Quality.HIGH,
-            Dimension.HUMIDITY: Quality.HIGH,
+            Dimension.UVS: Quality.HIGH,
+            Dimension.LIGHT: Quality.HIGH,
         }
         
     def attempt_connection(self, i2c):
         try:
-            self.sht31_device = adafruit_sht31d.SHT31D(i2c)
+            self.ltr = adafruit_ltr390.LTR390(i2c)
         except:
-            logger.debug("SHT31 sensor not detected")
+            logger.debug("LTR390 sensor not detected")
             return False
 
-        logger.debug(f"SHT31 device found on I2C bus {i2c}")
+        logger.debug(f"LTR390 device found on I2C bus {i2c}")
         return True
 
     def read(self):
         try:
             self.current_values = {
-                Dimension.TEMPERATURE: self.sht31_device.temperature,
-                Dimension.HUMIDITY: self.sht31_device.relative_humidity,
+                Dimension.UVS: self.ltr.uvs,
+                Dimension.LIGHT: self.ltr.light,
             }
         except:
-            logger.error("SHT31 Error")
+            logger.error("LTR390 Error")
