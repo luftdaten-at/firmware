4cf220b25cc13e36047880f5358a8f7796c744f3
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-29 11:15:09 +0100

    moved api methods to ld_product_model

--- firmware/models/ld_product_model.py
+++ firmware/models/ld_product_model.py
@@ -1,4 +1,7 @@
 import time
+import storage
+import json
+import os
 
 from logger import logger
 from wifi_client import WifiUtil
@@ -43,8 +46,21 @@ class LdProductModel:
 
     def get_initial_info(self):
         """
-        returns station info json for datahub status
+        returns station info json with additional sensor inforation for datahub status
         """
+        device_info = self.get_info()
+        # add list of all connected sensors
+        device_info['station']['sensor_list'] = [
+            {
+                "model_id": sensor.model_id,
+                "dimension_list": sensor.measures_values
+            } for sensor in self.sensors
+        ]
+
+        return device_info
+    
+    def get_info(self):
+        """returns json with device info for api"""
         current_time = time.localtime()
         formatted_time = f"{current_time.tm_year:04}-{current_time.tm_mon:02}-{current_time.tm_mday:02}T{current_time.tm_hour:02}:{current_time.tm_min:02}:{current_time.tm_sec:02}.000Z"
 
@@ -55,19 +71,89 @@ class LdProductModel:
                 "firmware": f"{Config.settings['FIRMWARE_MAJOR']}.{Config.settings['FIRMWARE_MINOR']}.{Config.settings['FIRMWARE_PATCH']}",
                 "model": Config.settings['MODEL'],
                 "apikey": Config.settings['api_key'],
-                
-                # list of all connected sensors
-                "sensor_list": [
-                    {
-                        "model_id": sensor.model_id,
-                        "dimension_list": sensor.measures_values
-                    } for sensor in self.sensors
-                ]
+                "source": 1,
             },
             "sensors": {}
         }
 
         return device_info
+    
+    def save_data(self, data: dict, tag = 'normal'):
+        current_time = time.localtime()
+        formatted_time = f"{current_time.tm_year:04}-{current_time.tm_mon:02}-{current_time.tm_mday:02}T{current_time.tm_hour:02}:{current_time.tm_min:02}:{current_time.tm_sec:02}.000Z"
+        storage.remount('/', False) 
+        file_name = formatted_time.replace(':', '_').replace('.', '_')
+        with open(f'{Config.runtime_settings["JSON_QUEUE"]}/{file_name}_{tag}.json', 'w') as f:
+            json.dump(data, f)
+        storage.remount('/', False)
+    
+    def get_json(self):
+        self.read_all_sensors()
+        sensor_values = {}
+        for id, sensor in enumerate(self.sensors):
+            sensor_values[id] = {
+                "type": sensor.model_id,
+                "data": sensor.current_values
+            }
+
+        data = self.get_info()
+        data["sensors"] = sensor_values
+
+        return data
+    
+    def send_to_api(self):
+        for file_path in (f'{Config.runtime_settings["JSON_QUEUE"]}/{f}' for f in os.listdir(Config.runtime_settings["JSON_QUEUE"])):
+            logger.debug(f'process file: {file_path}')
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+
+                if 'tmp_log.txt' in file_path:
+                    # send status to Luftdaten APi
+                    status_list = []
+                    for line in f.readlines():
+                        status_list.append(json.loads(line))
+
+                    data = self.get_info()
+                    data["status_list"] = status_list
+
+                    response = WifiUtil.send_json_to_api(data, router='status')
+                    logger.debug(f'{file_path=}')
+                    logger.debug(f'API Response: {response.status_code}')
+                    logger.debug(f'API Response: {response.text}')
+                    if response.status_code == 200:  # Placeholder for successful sending check
+                        storage.remount('/', False)
+                        os.remove(file_path) 
+                        storage.remount('/', True)
+                elif 'sensor_community' in file_path:
+                    # data = List[Tuple(header, data)]
+                    for header, d in data:
+                        response = WifiUtil.send_json_to_sensor_community(header=header, data=d)
+                        logger.debug(f'{file_path=}')
+                        logger.debug(f'API Response: {response.status_code}')
+                        logger.debug(f'API Response: {response.text}')
+                        if response.status_code != 200:
+                            break
+                    else:
+                        storage.remount('/', False)
+                        os.remove(file_path) 
+                        storage.remount('/', True)
+                else:
+                    # send to Luftdaten APi
+                    response = WifiUtil.send_json_to_api(data)
+                    logger.debug(f'{file_path=}')
+                    logger.debug(f'API Response: {response.status_code}')
+                    logger.debug(f'API Response: {response.text}')
+                    if response.status_code in (200, 422):  # Placeholder for successful sending check
+                        storage.remount('/', False)
+                        os.remove(file_path) 
+                        storage.remount('/', True)
+
+    def read_all_sensors(self):
+        for sensor in self.sensors:
+            try:
+                sensor.read()
+            except:
+                logger.error(f"Error reading sensor {sensor.model_id}, using previous values")
 
         
     def receive_command(self, command):
